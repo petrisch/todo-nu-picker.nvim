9fb8b7db3589c1e90428199480d76a8eb2781d99
patrick.joerg <patrick.joerg@gilgends.com>
2024-11-08 15:46:10 +0100

    Autoformat, added `/` to wiki path, added items to ordinal. Now fzf not only fuzzes through filenames.

--- lua/telescope/_extensions/todo_nu_picker.lua
+++ lua/telescope/_extensions/todo_nu_picker.lua
@@ -1,62 +1,64 @@
-local pickers = require "telescope.pickers"
-local finders = require "telescope.finders"
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
 local conf = require("telescope.config").values
-local utils = require "telescope.utils"
+local utils = require("telescope.utils")
 
 local M = {}
 
 M.setup = function(setup_config)
-    nu_config = setup_config.nu_config or "~/.config/nushell/config.nu"
-    wiki_path = setup_config.wiki_path or "~/wiki"
+	Nu_config = setup_config.nu_config or "~/.config/nushell/config.nu"
+	Wiki_path = setup_config.wiki_path or "~/wiki/"
 end
 
 local function get_todos(opts)
-  local cmd = "--config " .. nu_config .. " -c 'td | to json'"
-  opts.cmd = {"nu", cmd}
-  local response =  utils.get_os_command_output(opts.cmd)
-  local res = ""
-  for i, v in ipairs(response) do
-    res = res .. v
-  end
-  return vim.json.decode(res)
+	local cmd = "--config " .. Nu_config .. " -c 'td | to json'"
+	opts.cmd = { "nu", cmd }
+	local response = utils.get_os_command_output(opts.cmd)
+	local res = ""
+	for i, v in ipairs(response) do
+		res = res .. v
+	end
+	return vim.json.decode(res)
 end
 
 -- our picker function:
 M.todo_nu_picker = function(opts)
-  opts = opts or {}
+	opts = opts or {}
 
-  pickers.new(opts, {
-    prompt_title = "todo-nu",
-    results_title = "todo-nu",
-    finder = finders.new_table {
-      results = get_todos(opts),
-      entry_maker = function(entry) -- gets called for every entry
-        return {
-          value = entry,
-          display = entry["todo"] .. " " .. entry["file"] .. " | " .. entry["item"], -- Text as well as the icon get displayed
-          ordinal = entry["file"], -- the values for which they get sorted
-          filename = wiki_path .. entry["file"] .. '.md', -- The file to open
-          lnum = tonumber(entry["line"]), -- at this line number
-        }
-      end,
-    },
-    -- previewer = previewers.get_previewer(opts),
-    sorter = conf.generic_sorter(opts),
-    -- attach_mappings = function(prompt_bufnr, map)
-    --   actions.select_default:replace(function()
-    --     actions.close(prompt_bufnr)
-    --     local selection = action_state.get_selected_entry()
-    --     vim.api.nvim_put({ selection[1] }, "", false, true)
-    --   end)
-    --   return true
-    -- end,
-  }):find()
+	pickers
+		.new(opts, {
+			prompt_title = "todo-nu",
+			results_title = "todo-nu",
+			finder = finders.new_table({
+				results = get_todos(opts),
+				entry_maker = function(entry) -- gets called for every entry
+					return {
+						value = entry,
+						display = entry["todo"] .. " " .. entry["file"] .. " | " .. entry["item"], -- Text as well as the icon get displayed
+						ordinal = entry["file"] .. entry["item"], -- the values for which they get sorted
+						filename = Wiki_path .. entry["file"] .. ".md", -- The file to open
+						lnum = tonumber(entry["line"]), -- at this line number
+					}
+				end,
+			}),
+			-- previewer = previewers.get_previewer(opts),
+			sorter = conf.generic_sorter(opts),
+			-- attach_mappings = function(prompt_bufnr, map)
+			--   actions.select_default:replace(function()
+			--     actions.close(prompt_bufnr)
+			--     local selection = action_state.get_selected_entry()
+			--     vim.api.nvim_put({ selection[1] }, "", false, true)
+			--   end)
+			--   return true
+			-- end,
+		})
+		:find()
 end
 
-return require("telescope").register_extension {
-  setup = M.setup,
+return require("telescope").register_extension({
+	setup = M.setup,
 
-  exports = {
-    todo_nu_picker = M.todo_nu_picker,
-  },
-}
+	exports = {
+		todo_nu_picker = M.todo_nu_picker,
+	},
+})
