b69178e1df2500b627383ff48b30dff73c3c8cef
patrick.joerg <patrickjoerg@gmx.ch>
2026-04-19 15:49:01 +0200

    WIP now at least get defaults into config

--- lua/fzf-lua/fzf-lua-picker.lua
+++ lua/fzf-lua/fzf-lua-picker.lua
@@ -1,11 +1,12 @@
--- local utils = require("telescope.utils")
+-- Get todos with fzf.lua.
+-- https://github.com/ibhagwan/fzf-lua/wiki/Advanced#fzf-exec-api
 
 local M = {}
 
 M.setup = function(todo_opts)
 	Nu_config = todo_opts.nu_config or "~/.config/nushell/config.nu"
 	Wiki_path = todo_opts.wiki_path or "~/wiki"
-	Snacks.notifier.notify("setup fzf: " .. Nu_config, "warn")
+	-- Snacks.notifier.notify("setup fzf: " .. Nu_config, "warn")
 
 	function Pick()
 		M.fzf_pick_todo(todo_opts)
@@ -15,29 +16,20 @@ M.setup = function(todo_opts)
 end
 
 M.fzf_pick_todo = function(todo_opts)
-	Snacks.notifier.notify("nu config: " .. todo_opts.nu_config, "warn")
+	-- Snacks.notifier.notify("nu config: " .. todo_opts.nu_config, "warn")
 	local fzf_lua = require("fzf-lua")
-	-- opts = vim.tbl_deep_extend("error", opts, fzf_config)
-	---@class
-	local opts = fzf_lua.config
+	local fzf_config = vim.deepcopy(fzf_lua.config or {})
+	-- Snacks.notifier.notify("fzf_config: " .. vim.inspect(fzf_config), "warn")
 
-	opts.show_quickfix = true
-	opts.prompt = "todos> "
+	local fzf_actions_files = vim.deepcopy(fzf_config.defaults.actions.files or {})
 
-	Snacks.notifier.notify("fzf opts: " .. vim.inspect(opts), "warn")
-	Snacks.notifier.notify("fzf opts.show_quickfix: " .. vim.inspect(opts.show_quickfix), "warn")
-
-	opts.fn_transform = function(x)
-		local res = string.gsub(x, "\t", " | ")
-		return res
-	end
-	opts.actions = {
-		["default"] = function(selected)
+	local my_actions = {
+		default = function(selected)
 			local i = 0
 			local file = ""
 			local lnum = ""
 			for entry in string.gmatch(selected[1], "[^|]+") do
-				Snacks.notifier.notify("entry: " .. entry, "warn")
+				-- Snacks.notifier.notify("entry: " .. entry, "warn")
 				i = i + 1
 				if i == 3 then
 					file = vim.fs.normalize(todo_opts.wiki_path .. string.gsub(entry, "%s+", "") .. ".md")
@@ -49,6 +41,22 @@ M.fzf_pick_todo = function(todo_opts)
 			vim.cmd("e" .. file .. " | " .. lnum)
 		end,
 	}
+
+	local actions = vim.tbl_deep_extend("force", fzf_actions_files, my_actions)
+
+	local config = {
+		-- show_quickfix = true,
+		prompt = "todos> ",
+		fn_transform = function(x)
+			local res = string.gsub(x, "\t", " | ")
+			return res
+		end,
+		actions = actions,
+	}
+	Snacks.notifier.notify("config: " .. vim.inspect(config), "warn")
+
+	local opts = vim.tbl_deep_extend("force", fzf_config, config)
+
 	local cmd = "nu " .. "--config " .. todo_opts.nu_config .. " -c 'td | to tsv | lines | skip 1 | to text'"
 	-- Snacks.notifier.notify("cmd todo fzf: " .. cmd, "warn")
 	fzf_lua.fzf_exec(cmd, opts)
