b1b6a408d9b71861be6971ad316f2cf97132f754
patrick.joerg <patrickjoerg@gmx.ch>
2026-04-19 13:15:15 +0200

    WIP to get quickfix stuff to work with fzf

--- lua/fzf-lua/fzf-lua-picker.lua
+++ lua/fzf-lua/fzf-lua-picker.lua
@@ -1,11 +1,11 @@
-local utils = require("telescope.utils")
+-- local utils = require("telescope.utils")
 
 local M = {}
 
 M.setup = function(todo_opts)
 	Nu_config = todo_opts.nu_config or "~/.config/nushell/config.nu"
 	Wiki_path = todo_opts.wiki_path or "~/wiki"
-	-- Snacks.notifier.notify("setup fzf: " .. Nu_config, "warn")
+	Snacks.notifier.notify("setup fzf: " .. Nu_config, "warn")
 
 	function Pick()
 		M.fzf_pick_todo(todo_opts)
@@ -14,10 +14,19 @@ M.setup = function(todo_opts)
 	vim.keymap.set("n", "<leader>fd", Pick, { silent = true, desc = "Get todos" })
 end
 
-M.fzf_pick_todo = function(opts)
-	opts = opts or {}
+M.fzf_pick_todo = function(todo_opts)
+	Snacks.notifier.notify("nu config: " .. todo_opts.nu_config, "warn")
 	local fzf_lua = require("fzf-lua")
+	-- opts = vim.tbl_deep_extend("error", opts, fzf_config)
+	---@class
+	local opts = fzf_lua.config
+
+	opts.show_quickfix = true
 	opts.prompt = "todos> "
+
+	Snacks.notifier.notify("fzf opts: " .. vim.inspect(opts), "warn")
+	Snacks.notifier.notify("fzf opts.show_quickfix: " .. vim.inspect(opts.show_quickfix), "warn")
+
 	opts.fn_transform = function(x)
 		local res = string.gsub(x, "\t", " | ")
 		return res
@@ -28,9 +37,10 @@ M.fzf_pick_todo = function(opts)
 			local file = ""
 			local lnum = ""
 			for entry in string.gmatch(selected[1], "[^|]+") do
+				Snacks.notifier.notify("entry: " .. entry, "warn")
 				i = i + 1
 				if i == 3 then
-					file = vim.fs.normalize(opts.wiki_path .. string.gsub(entry, "%s+", "") .. ".md")
+					file = vim.fs.normalize(todo_opts.wiki_path .. string.gsub(entry, "%s+", "") .. ".md")
 				end
 				if i == 4 then
 					lnum = entry
@@ -39,7 +49,8 @@ M.fzf_pick_todo = function(opts)
 			vim.cmd("e" .. file .. " | " .. lnum)
 		end,
 	}
-	local cmd = "nu " .. "--config " .. opts.nu_config .. " -c 'td | to tsv | lines | skip 1 | to text'"
+	local cmd = "nu " .. "--config " .. todo_opts.nu_config .. " -c 'td | to tsv | lines | skip 1 | to text'"
+	-- Snacks.notifier.notify("cmd todo fzf: " .. cmd, "warn")
 	fzf_lua.fzf_exec(cmd, opts)
 end
 
